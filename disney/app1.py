06cfe9fb31c89cb4366f5816c7b83f4e811b3c4a
Tanisha_Bharol <46688469+tubbytani@users.noreply.github.com>
2021-08-22 15:42:51 +0530

    Update app1.py

--- disney/app1.py
+++ disney/app1.py
@@ -11,6 +11,9 @@ app = Flask(__name__)
 CORS(app)
 @app.route('/', methods=['POST'])
 def func():
+    '''
+    This function recieves the request and sends back the output response to the web app
+    '''
     string = json.loads(request.data)
     time.sleep(1)
     try:
@@ -18,6 +21,7 @@ def func():
         time.sleep(2)
     except:
         ans = "tweepy couldnt handle request"
+        # Error handling in case tweepy couldn't process the query
     response = jsonify(ans)
     response.headers.add("Access-Control-Allow-Origin", "*")
     return response 
