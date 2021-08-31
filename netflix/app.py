f90822c400c311c961ed84c1c0749c50467aaded
Tanisha_Bharol <46688469+tubbytani@users.noreply.github.com>
2021-08-31 11:35:48 +0530

    Update app.py

--- netflix/app.py
+++ netflix/app.py
@@ -1,6 +1,5 @@
 from Sentiment_netflix import final
 from flask import Flask, jsonify , request
-#from werkzeug.utils import cached_property
 from flask_restx import Api
 from flask_cors import CORS
 from gevent.pywsgi import WSGIServer
@@ -15,14 +14,15 @@ def func():
     string = json.loads(request.data)
     time.sleep(1)
     try:
+        # Calling the main python function for output
         ans = final() 
         time.sleep(2)
     except:
+        # Exception handling incase tweepy cannot handle requests.
         ans = "tweepy couldnt handle request"
     response = jsonify(ans)
     response.headers.add("Access-Control-Allow-Origin", "*")
     return response 
-#port = os.getenv('VCAP_APP_PORT','8080')
 
 if __name__ == "__main__": 
     app.run(debug=True)
